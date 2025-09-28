6b458a4aa2424e491a55d8e86cf2ad142efe0bcf
noah-quetua <noah.quetua@stu.ocsb.ca>
2025-09-28 17:45:10 +0000

    added main.ts for string literal

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Noah Quetua
+ * @version 1.0.0
+ * @date 2025-09-28
+ * @fileoverview This program practices wiht string literals.
+ */
+
+// string literal text 
+console.log("I love Computer Science!!");
+
+// string literal text
+console.log("    I  love Computer Science");
+
+// string literal text
+console.log("I    love   Computer   Science   100%");
+
+console.log("\nDone");
